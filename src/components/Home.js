ba4b7dd79caf510a90ccce2bae91f55ab9975d8c
Rohan Rana <rohanrana.mail@gmail.com>
2023-04-27 11:27:34 +0530

    add environment variables

--- src/components/Home.js
+++ src/components/Home.js
@@ -13,8 +13,8 @@ const Home = () => {
     const [wind, setWind] = useState("");
     const [clouds, setClouds] = useState("");
 
-    // const apiKey = process.env.REACT_APP_WEATHER_API;
-    const apiKey = '1b6ef03eab26715d8675d1633bf6eb6e';
+    const apiKey = process.env.REACT_APP_WEATHER_API;
+    // const apiKey = '1b6ef03eab26715d8675d1633bf6eb6e';
 
     const fetchAPI = async () => {
         const CoordinatesUrl = `https://api.openweathermap.org/geo/1.0/direct?q=${search}&appid=${apiKey}`
